49770fe6050359b414460d7d92124112f4104045
Oleg Zolotar <zolotar1123@gmail.com>
2015-09-07 16:58:54 +0300

    Optimize import

--- app/src/main/java/com/example/d_rom/supportdesigndemo/SecondActivity.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/SecondActivity.java
@@ -1,11 +1,9 @@
 package com.example.d_rom.supportdesigndemo;
 
+import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
--- app/src/main/java/com/example/d_rom/supportdesigndemo/SimpleListFragment.java
+++ app/src/main/java/com/example/d_rom/supportdesigndemo/SimpleListFragment.java
@@ -1,10 +1,8 @@
 package com.example.d_rom.supportdesigndemo;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
